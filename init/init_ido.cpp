678a8ec80b86cf83acb8057974e192291220c60d
Danny Baumann <dannybaumann@web.de>
2018-08-31 13:34:18 +0200

    msm8916-common: Load dummy initial BT MAC address.
    The real one will be loaded by hci_qcomm_init, but the BT HAL wants an
    address to be set before that point.
    
    Change-Id: Ia8ff499ccaa862ad697dcfecb969b29f29ee89f4

--- init/init_ido.cpp
+++ init/init_ido.cpp
@@ -124,5 +124,8 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", "8m");
 
+    // Init a dummy BT MAC address, will be overwritten later
+    property_set("ro.boot.btmacaddr", "00:00:00:00:00:00");
+
     init_alarm_boot_properties();
 }
