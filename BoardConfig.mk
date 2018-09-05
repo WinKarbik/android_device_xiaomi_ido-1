a230b9a1b4ad3c74f9a9898df5950c7223af5891
Bruno Martins <bgcngm@gmail.com>
2018-09-05 23:01:52 +0100

    ido: Include only legacy QC sepolicy
     * This has now turned into a separate repository.
    
    Change-Id: I981b452b697bc3610d7aa97b74ed182c6b70ca30

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,8 +162,7 @@ USE_OPENGL_RENDERER := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
