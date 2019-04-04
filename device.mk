9f46f125be71f696f9e3790c32075d660396872b
WinKarbik <karbainovs@mail.ru>
2019-04-04 18:38:42 +0300

    ido: Use binnderized light HAL

--- device.mk
+++ device.mk
@@ -213,7 +213,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    lights.msm8916
+    android.hardware.light@2.0-service.aw2013
 
 # Media
 PRODUCT_COPY_FILES += \
