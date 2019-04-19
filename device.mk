0189fb39c22a91007d734a92deb4ba90164813b4
WinKarbik <karbainovs@mail.ru>
2019-04-19 08:09:25 +0300

    ido: Use power hal from cyngn msm8916-common
    Signed-off-by: WinKarbik <karbainovs@mail.ru>

--- device.mk
+++ device.mk
@@ -247,9 +247,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8916
+    android.hardware.power@1.1-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
