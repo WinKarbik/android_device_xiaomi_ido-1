b82c4bda0390f1fe3e9bf88667ac066b12dad504
WinKarbik <karbainovs@mail.ru>
2019-07-01 12:25:47 +0300

    ido: Selinux enforcing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,7 +42,6 @@ TARGET_NO_BOOTLOADER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02000000
